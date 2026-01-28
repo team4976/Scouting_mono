2e94577395878e71e330152e9343ee96c71978cc
rikkkkkkkkii <funkysilverbutterfly@gmail.com>
2026-01-27 20:30:05 -0500

    AHHHHHHHHHHHHH

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,81 @@ export function App() {
       <div className="title">
         Home Page
       </div>
-      <button className="navBut">
-       Start
-      </button>
-      <div className="row">
-        <button className="navBut">
-          Settings
-        </button>
-      </div>
-      <div className="row">
-        <button className="navBut">
-          History
-        </button>
-      </div>
+    <Startbut></Startbut>
+       <Setbut></Setbut>
+        <Histbut></Histbut>
       <div className="title">
         Pre-Game
       </div>
+      <Textbox></Textbox>
+      <Textbox></Textbox>
+      <div className="spacer2">
+        Team Colour
+      </div>
+      <CheckboxT></CheckboxT>
+      <div className="spacer2">
+      No Show
+      </div>
+      <CheckboxG></CheckboxG>
+      <div className="title">
+        Auto
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
+export function Startbut(){
+  return (<button className="navBut">
+        Start
+        </button>)
+
+}
+
+export function Setbut(){
+  return ( <button className="navBut">
+          Settings
+        </button>)
+}
+
+export function Histbut(){
+  return (<button className="navBut">
+          History
+        </button>)
+}
+
+export function Textbox(){
+  return (
+    <input
+      type="text"
+      placeholder="wfaoajsf"
+      className="inputText"/>
+  )
+}
+
+export function CheckboxT(){
+  return (
+     <div className="row"><input 
+      type="checkbox"
+      className="checkboxB"/>
+      <input
+      type="checkbox"
+      className="checkboxR"/>
+      </div>
+)
+}
+
+export function CheckboxG(){
+  return(
+    <input
+    type="checkbox"
+    className="checkboxG">
+    </input>
+  )
+}
+
+
+
+

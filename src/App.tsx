25c3546655c0e914537bd2f23db206e7b1c175af
Harrison-Furg <harrisonferguson36@gmail.com>
2026-02-28 10:23:15 -0500

    updated version numbers

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export function App() {
   }, [])
   return (          
     <div className="screen">
-      Ver 1
+      Ver 2
       <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage/>} />
